4e80f30270c4600f86477cf5dabaa972e7ba238f
TgeorgeT <76885076+TgeorgeT@users.noreply.github.com>
2022-12-12 14:58:20 +0200

    Add files via upload

---
+++ check.php
@@ -0,0 +1,41 @@
+<?php
+
+    function check(){
+    
+           $user_ag = $_SERVER['HTTP_USER_AGENT'];
+           if(preg_match('/(Mobile|Android|Tablet|GoBrowser|[0-9]x[0-9]*|uZardWeb\/|Mini|Doris\/|Skyfire\/|iPhone|Fennec\/|Maemo|Iris\/|CLDC\-|Mobi\/)/uis',$user_ag)){
+            echo '<script type="text/javascript">
+            alert("Mobil");
+            </script>';
+           }
+           else {
+           echo ' <script type="text/javascript">
+            alert("Desktop");
+            </script>';
+           };
+        };
+       
+    
+    
+?>
+
+<!DOCTYPE html>
+<html>
+<body style="text-align:center;">
+     
+
+    <?php
+        if(array_key_exists('button1', $_POST)) {
+            check();
+        }
+
+    ?>
+ 
+    <form method="post">
+        <input type="submit" name="button1"
+                class="button" value="Afiseaza tipul" />
+        
+    </form>
+</body>
+ 
+</html>
