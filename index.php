e59e48be26fda001684992b2def2954fb2a78d8a
TgeorgeT <76885076+TgeorgeT@users.noreply.github.com>
2022-12-15 21:07:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -23,11 +23,12 @@
             'SM',
             'EPOC',
             ); 
+           
            $user_ag = $_SERVER['HTTP_USER_AGENT'];
+            echo $user_ag;
            if(preg_match('/(Mobile|Android|Tablet|GoBrowser|[0-9]x[0-9]*|uZardWeb\/|Mini|Doris\/|Skyfire\/|iPhone|Fennec\/|Maemo|Iris\/|CLDC\-|Mobi\/)/uis',$user_ag)){
             echo '<h4>';
             echo "Mobil<br>";
-            
             if($first=strstr( $user_ag, 'Android')){
                 
                 foreach ( $useragents as $useragents ) { 
